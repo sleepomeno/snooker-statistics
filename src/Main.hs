24b2b20347b1400ca1355eee256060fbec2d9b5c
Gregor Riegler <gregor.riegler@gmail.com>
2014-07-02 14:01:38 +0200

    When you login read the login information from a property file

--- src/Main.hs
+++ src/Main.hs
@@ -1,29 +1,47 @@
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
-{-# LANGUAGE StandaloneDeriving         #-}
 
 module Main where
 
-import           Control.Concurrent     (threadDelay)
-import           Control.Monad          (void)
-import           Control.Monad.IO.Class (MonadIO, liftIO)
-import           Control.Monad.Trans    (lift)
-import           Data.Text              (pack, unpack)
-import           Test.WebDriver
+import Control.Concurrent (threadDelay)
+import Control.Monad (void,join,liftM)
+import Control.Monad.Error (runErrorT,ErrorT)
+import Control.Monad.IO.Class (MonadIO, liftIO)
+import Control.Monad.Trans (lift)
+import Data.ConfigFile
+import Data.Either.Utils
+import Data.Text (pack, unpack)
+import Paths
+import System.FilePath
+import Test.WebDriver
+
 
 main :: IO ()
 main = let conf = defaultCaps { browser = chrome } in
-  void $ runSession defaultSession conf $
+  void $ do
+  dataDir <- getStaticDir
+  let configFile = dataDir </> "config.txt"
+
+  eitherConfig <- runErrorT $ do
+    parser <- join $ liftIO $ readfile emptyCP configFile
+    loginUser <- get parser "DEFAULT" "loginuser"
+    loginPwd <- get parser "DEFAULT" "loginpwd"
+    return (loginUser, loginPwd) :: ErrorT CPError IO (String, String)
+
+  let (loginUser, loginPwd) = forceEither eitherConfig
+
+  runSession defaultSession conf $
        do openPage "http://www.gamedesire.com"
+
           setImplicitWait 3000
           loginBtn <- findElem $ ByClass (pack "login_button")
           click loginBtn
           liftIO $ threadDelay 3500
-          loginOverlay <- findElem $ ById (pack "overlay_login_box")
+          _ <- findElem $ ById (pack "overlay_login_box")
           name <- findElem $ ById (pack "userLogin")
           password <- findElem $ ById (pack "user_passwd")
           loginForm <- findElem $ ById (pack "loginForm")
-          sendKeys (pack "username") name
-          sendKeys (pack "password") password
+          sendKeys (pack loginUser) name
+          sendKeys (pack loginPwd) password
           submit loginForm
           liftIO $ threadDelay 3500000
           openPage "http://www.gamedesire.com/#/?dd=1&n=0&mod_name=player_results&sub=1&view=edit&gg=103"
---
+++ src/Paths.hs
@@ -0,0 +1,20 @@
+{-# LANGUAGE CPP#-}
+module Paths (getStaticDir) where
+
+import Control.Monad
+import System.FilePath
+
+#if CABAL
+-- using cabal
+import qualified Paths_snooker_statistics (getDataDir)
+
+getStaticDir :: IO FilePath
+getStaticDir = (</> "data") `liftM` Paths_sudoku.getDataDir
+
+#else
+-- using GHCi
+
+getStaticDir :: IO FilePath
+getStaticDir = return "data/"
+
+#endif
